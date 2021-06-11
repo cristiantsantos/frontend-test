94bf0722e47953b6d4b49a05f13de956f5186185
Cristian <cristian.tsantos@hotmail.com>
2021-06-11 09:01:27 -0300

    Carregando Dados da api

--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../services/api';
 
 export default function Main() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function loadPosts() {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch (err) {
+      }
+    }
+    loadPosts();
+  }, []);
+
   return (
-    <h1>Main</h1>
+    <>
+      { posts.map((post) => (
+        <h1 key={post.id}>
+          {post.userId} -
+          {post.title} -
+          {post.body}
+        </h1>
+      ))}
+    </>
   );
 }
--- front/src/pages/Post.js
+++ front/src/pages/Post.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from '../services/api';
 
 export default function Post() {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    async function loadComments(postId) {
+      try {
+        const response = await api.get('/comments');
+        const postFilter = response.data.filter(x => x.postId === postId)
+        setComments(postFilter);
+      } catch (err) {
+      }
+    }
+    loadComments(2);
+  }, []);
+
   return (
-    <h1>Post</h1>
+    <>
+      { comments.map((comment) => (
+        <h1 key={comment.id}>
+          {comment.name} -
+          {comment.body}
+        </h1>
+      ))}
+    </>
   );
 }
